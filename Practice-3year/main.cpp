158a7c498a9edb3ca2c425e098a46ab3f1b3c654
Anatolii Myachev <anatoliimyachev@mail.ru>
2018-02-13 00:20:08 +0300

    13.02.18

--- Practice-3year/main.cpp
+++ Practice-3year/main.cpp
@@ -5,13 +5,22 @@
 /*numbering of vertices from the number one*/
 
 int main() {
-	std::ifstream fin("C:\\example\\practice.txt");
+	std::ifstream fin("D:\\practice.txt");
 	std::istream_iterator<int16_t> iter(fin);
 	graph grh(iter);
 	characteristics v_chr(iter);
 	if (verify_input_data(grh, v_chr) == false) std::cout << "input data do not satisfy the restriction of injection" << std::endl;
 	grh.print();
 	std::sort(v_chr.begin(), v_chr.end(), [](int first, int second) {return first < second; });
+	std::vector<appointment> v_app;
+	for (int i = 0; i < v_chr.size(); ++i)
+		std::cout << v_chr[i] << " ";
+	std::cout << std::endl;
+	v_app.push_back(appointment(1, 1));
+	method_branches_borders alg(&grh, &v_chr);
+	alg.lower_bound(v_app);
+
+
 	
 	/*try {
 
